555c82088d0aa900b115dd990b35343999167681
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-02-08 16:32:16 +0100

    Fix controller logic and remove unnecesary file

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,15 +4,13 @@ import checks from "../middleware/checks.js";
 
 const router = Router();
 
-router.get("/", checks.isLoggedIn, (_, res) => res.sendStatus(200));
-router.post("/", controller.login, (_, res) =>
-    res.status(200).json({ data: "WELCOME BACK APE" }),
+router.get("/", checks.isLoggedIn, (req, res) =>
+    res.status(200).json({ username: req.user.username, data: "logged in" }),
 );
-router.get("/admin", checks.isLoggedIn, checks.isAdmin, (_, res) =>
-    res.sendStatus(200),
+router.post("/", controller.login, (req, res) =>
+    res.status(200).json({ username: req.user.username, data: "WELCOME BACK APE" }),
 );
-router.post("/admin", controller.login, checks.isAdmin, (_, res) =>
-    res.sendStatus(200),
-);
+router.get("/admin", checks.isLoggedIn, checks.isAdmin, (_, res) => res.sendStatus(200));
+router.post("/admin", controller.login, checks.isAdmin, (_, res) => res.sendStatus(200));
 
 export default router;
